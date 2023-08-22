fc0e0075ec8e43ae23d4ace850b648f421e2be86
KsW96 <ghkfzha1@gmail.com>
2023-08-22 11:18:06 +0900

    db propertise 확인하세요~ dungsmall DB도 이슈에 다시 올려놨어요

--- DungsMall/src/order/command/OrderHandler.java
+++ DungsMall/src/order/command/OrderHandler.java
@@ -29,7 +29,6 @@ import order.service.OrderService;
 public class OrderHandler implements CommandHandler {
 	private static final String FORM_VIEW = "view/purchase/order/order.jsp";
 	private OrderService os = new OrderService();
-	private List<BasketList> list = new ArrayList<>();
 
 	@Override
 	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
@@ -38,7 +37,7 @@ public class OrderHandler implements CommandHandler {
 			return processForm(req, res);
 		} else if (req.getMethod().equalsIgnoreCase("POST")) {
 			System.out.println("Order 핸들러 Post 지나침!");
-			
+
 			return processSubmit(req, res);
 		} else {
 			res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
@@ -57,21 +56,21 @@ public class OrderHandler implements CommandHandler {
 		DetailService ds = new DetailService();
 		String sessionId = (String) req.getAttribute("sessionId");
 		String line;
-
+		List<BasketList> list = new ArrayList<>();
 
 		// JSON 데이터를 Gson을 이용하여 객체로 변환
 		Gson gson = new Gson();
 		BasketList[] basketItems = gson.fromJson(req.getParameter("postData"), BasketList[].class);
 
 		// 체크된 리스트 생성하기
-		if(basketItems != null) {
-		for (BasketList item : basketItems) {
-			list.add(new BasketList(item.getName(), item.getPrice(), item.getCount()));
-		}
-		
-		System.out.println("추가가 끝난 리스트" + list);
-		req.setAttribute("basketCheckList", list);
-		return FORM_VIEW;
+		if (basketItems != null) {
+			for (BasketList item : basketItems) {
+				list.add(new BasketList(item.getName(), item.getPrice(), item.getCount()));
+			}
+
+			System.out.println("추가가 끝난 리스트" + list);
+			req.setAttribute("basketCheckList", list);
+			return FORM_VIEW;
 		} else {
 			System.out.println("OrderHandler : 아직 아이템이 담기지 않았습니다.");
 		}
